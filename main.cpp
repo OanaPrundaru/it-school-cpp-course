55647caac597b184771554f35d4cdb12d526e0a8
OanaPrundaru <oany.valentina01@gmail.com>
2025-11-12 19:00:33 +0100

    numar par si impar(if)
    Added functionality to check if a number is even or odd.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
-#include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from cpp-dev!\n";
+
+#include <iostream>
+#include <cstdint>
+
+int main()
+{   
+    int input =0;
+     
+    std::cout <<" Introdu un numar: " <<std::endl;
+    std::cin >>input;
+    
+    std::cout << "Numarul : "<<std::endl;   
+    
+    int rest = input %2;
+    
+    if (rest ==0) { 
+        std::cout <<"Numarul este par!" <<std::endl;}
+    
+    else {
+        std::cout <<"Numarul este impar!" <<std::endl; }
+    
+    return 0;
 }
+
+
+
