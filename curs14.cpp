58a13852fac324048039ee602ba72d2082f16f9c
Oana Prundaru <oany.valentina01@gmail.com>
2026-01-14 18:22:11 +0100

    WIP

--- curs14.cpp
+++ curs14.cpp
@@ -1,2 +1,27 @@
+
 #include <iostream>
 
+class FortzaInt{
+public:
+    int value;
+    
+    FortzaInt(const int _value) : value(_value){}
+    FortzaInt() : FortzaInt(9000) {}
+
+    FortzaInt operator+ (const FortzaInt& other){
+        return FortzaInt(this->value * other.value);
+    }
+};
+
+
+int main()
+{
+    FortzaInt val0;
+    FortzaInt val1(20);
+
+    FortzaInt val2 = val0 +val1;
+    
+    
+
+    return 0;
+}
