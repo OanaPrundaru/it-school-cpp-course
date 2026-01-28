eb30ca2bb16424a110180e3a111d6dd79358ed35
Oana Prundaru <oany.valentina01@gmail.com>
2026-01-28 19:29:26 +0100

    W

--- curs17.cpp
+++ curs17.cpp
@@ -7,6 +7,7 @@
 // printeaza count-ul pentru caracterele intre a-z si A-Z care este != 0w
 
 
+// HISTOGRAM
 
 #include <iostream>
 #include <map>
@@ -55,3 +56,74 @@ int main() {
 printCharHistogram(result);
     return 0;
 }
+
+
+
+// int main()
+// {
+//     std::map<std::string, int> mapVariableName = {{"anotherkey", 23}};
+    
+//     mapVariableName["key"] = mapVariableName["key"] +1;
+//     std::cout <<mapVariableName["key"] <<std::endl;
+    
+//     for (const auto& [key,value] : mapVariableName) {
+//         std::cout << key << " : " << std::endl;
+//     }
+    
+//     std::cout << mapVariableName.contains("222key") << std::endl;
+    
+//     return 0;
+// }
+
+//MAP
+
+// #include <iostream>
+// #include <map>
+// #include <string>
+// #include <vector>
+
+
+//  std::map < char, int > mapVariableName(const std::string& data) {
+    
+//      std::map < char, int > mapVariableName;
+     
+//      for (size_t i=0; i< data.size(); i++) {
+     
+//         mapVariableName[data[i]]++;
+//     }
+
+//     return mapVariableName;
+// }
+
+
+//  void printmapVariableName(const std::map < char, int>& data){
+     
+//      int aux = 0;
+//         for(const auto& [letter ,count]:data){
+//             aux++;
+//             std::cout << aux << std::endl;
+//             if(count ==0){
+//                 continue;
+//             }
+            
+//            if(letter < 'A' || letter > 'z'){
+//                 continue;
+//             }
+//             if(letter > 'Z' && letter < 'a'){
+//                 continue;
+            
+//     }
+//             std::cout << " letter : " << letter << " : "<< count << std::endl;
+// }
+// }
+
+// int main() {
+//     std::string text = "Hello World!";
+//     std::map< char, int > result = mapVariableName(text);
+
+// printmapVariableName(result);
+//     return 0;
+// }
+
+
+
